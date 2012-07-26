23875f2fba7c74af2e09aa2a7073a35b5584504a
Nicolas Vanheuverzwijn <nvanheuverzwijn@kronostechnologies.com>
2012-07-26 09:38:15 -0400

    Fixed an error in sql query

--- include/KronosBasicAuthBackend.php
+++ include/KronosBasicAuthBackend.php
@@ -8,12 +8,11 @@ class KronosBasicAuthBackend extends Sabre_DAV_Auth_Backend_AbstractBasic{
 	protected $pdo;
 	
 	public function __construct(PDO $pdo){
-		parent::__construct();
 		$this->pdo = $pdo;
 	}
 	
 	public function validateUserPass($username, $password){
-		$stmt = $this->pdo->prepare('SELECT email, password FROM kronos_users WHERE username = ? AND SHA1(?) == password');
+		$stmt = $this->pdo->prepare('SELECT email, password FROM kronos_users WHERE email = ? AND SHA1(?) = password');
 		$stmt->execute(array($username, $password));
 		return $stmt->fetch();
 	}

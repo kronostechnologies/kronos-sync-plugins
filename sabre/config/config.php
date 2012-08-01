4269fcf95020dd22cad2b4b34b781a32056d50ec
Nicolas Vanheuverzwijn <nvanheuverzwijn@kronostechnologies.com>
2012-08-01 16:09:26 -0400

    Added zpush and sabre into subfolder.

---
+++ sabre/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('MYSQL_DBUSER', 'root');
+define('MYSQL_DBPASSWORD', '');
+define('MYSQL_DBNAME', 'caldav');
+define('MYSQL_DBHOST', 'localhost');
+define('SABREDAV_PRINCIPAL_PREFIX', 'principals');
